e1ce6c3f37014ba0cc262b8d876c19ade5bbd6ea
Muhamad Irwan <muhamadirwan99@gmail.com>
2021-08-31 15:19:42 +0700

    menambahkan tugas hari ke 5 (string dan kondisional)

---
+++ Tugas String/soal1.js
@@ -0,0 +1,11 @@
+var word = 'JavaScript '; 
+var second = 'is '; 
+var third = 'awesome '; 
+var fourth = 'and '; 
+var fifth = 'I '; 
+var sixth = 'love '; 
+var seventh = 'it!';
+
+var onesentences = word + second + third + fourth + fifth + sixth + seventh;
+
+console.log(onesentences);
